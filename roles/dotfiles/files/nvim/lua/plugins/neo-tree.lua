677f28c7685d1dcb89143f0d333242b68adf2e36
Kartik Vashistha <kartikv.work@gmail.com>
2024-05-19 21:37:57 +0100

    style: some linting changes

--- roles/dotfiles/files/nvim/lua/plugins/neo-tree.lua
+++ roles/dotfiles/files/nvim/lua/plugins/neo-tree.lua
@@ -1,26 +1,24 @@
 -- Neotree
 return {
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
 
-		cmd = "Neotree",
-		keys = {
-			{
-				"<leader>e",
-				function()
-					require("neo-tree.command").execute({
-						toggle = true,
-						reveal = true,
-					})
-				end,
-				desc = "Explorer NeoTree",
-			},
+	cmd = "Neotree",
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({
+					toggle = true,
+					reveal = true,
+				})
+			end,
+			desc = "Explorer NeoTree",
 		},
 	},
 }
