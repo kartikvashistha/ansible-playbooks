6a3722dc6293c656a4ba58f427fc8882ddcd80cf
Kartik Vashistha <kartikv.work@gmail.com>
2024-06-16 13:24:35 +0100

    feat: add cmp-path
    n

--- roles/neovim/files/nvim/lua/plugins/completion.lua
+++ roles/neovim/files/nvim/lua/plugins/completion.lua
@@ -47,4 +47,15 @@ return {
 			})
 		end,
 	},
+	{
+		"hrsh7th/cmp-path",
+		lazy = false,
+		config = function()
+			require("cmp").setup({
+				sources = {
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 }
