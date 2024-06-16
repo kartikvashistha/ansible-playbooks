620f44decf42813fa4edd9bb88191ce6739426e2
Kartik Vashistha <kartikv.work@gmail.com>
2024-06-16 13:26:40 +0100

    feat: use mini for statusline and refactor lua file name

---
+++ roles/neovim/files/nvim/lua/plugins/statusline.lua
@@ -0,0 +1,8 @@
+return {
+	-- "nvim-lualine/lualine.nvim",
+	-- dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- opts = {},
+	-- }
+
+	{ "echasnovski/mini.statusline", version = "*", opts = {} },
+}
