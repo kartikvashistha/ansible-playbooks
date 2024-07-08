b73354e5473061a4193f3a91c76f57d91b515863
Kartik Vashistha <kartikv.work@gmail.com>
2024-07-08 13:40:22 +0100

    fix: completion
    cmp wasn't being triggered, so fixing that.

--- roles/neovim/files/nvim/lua/plugins/completion.lua
+++ roles/neovim/files/nvim/lua/plugins/completion.lua
@@ -2,25 +2,27 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		lazy = false,
-		run = "make install_jsregexp",
+		build = "make install_jsregexp",
 		dependencies = {
-			"rafamadriz/friendly-snippets",
-			"saadparwaiz1/cmp_luasnip",
+			{
+				"rafamadriz/friendly-snippets",
+				config = function()
+					require("luasnip.loaders.from_vscode").lazy_load()
+				end,
+			},
 		},
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		lazy = false,
-		config = true,
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-path",
+		},
 		config = function()
 			local cmp = require("cmp")
+
 			cmp.setup({
 				window = {
 					documentation = cmp.config.window.bordered(),
@@ -31,6 +33,9 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+
+				completion = { completeopt = "menu,menuone,noinsert" },
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -41,20 +46,10 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+					{ name = "path" },
 				}, {
 					{ name = "buffer" },
 				}),
-			})
-		end,
-	},
-	{
-		"hrsh7th/cmp-path",
-		lazy = false,
-		config = function()
-			require("cmp").setup({
-				sources = {
-					{ name = "path" },
-				},
 			})
 		end,
 	},
