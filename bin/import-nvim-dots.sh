267ce343086788e125ee704734219ce9db8c9bec
Kartik Vashistha <kartikv.work@gmail.com>
2025-05-17 23:18:45 +0100

    feat: update script to also include the lsp folder under nvim rtp

--- bin/import-nvim-dots.sh
+++ bin/import-nvim-dots.sh
@@ -4,5 +4,4 @@
 set -e
 export NVIM_FILES_PATH="$HOME/code/personal/ansible-playbooks/roles/neovim/files/nvim"
 rm -r $NVIM_FILES_PATH/*
-cp -r ~/.config/nvim/{"init.lua",lua/} $NVIM_FILES_PATH
-
+cp -r ~/.config/nvim/{"init.lua",lua/,lsp/} $NVIM_FILES_PATH
