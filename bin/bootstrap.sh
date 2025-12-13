77a50906be4e0e541ef8e47ed656efd63bb2b71d
Kartik Vashistha <kartikv.work@gmail.com>
2025-12-13 15:31:59 +0000

    feat: do more bootstrapping for fedora
    Install 1password, rpmfusion and ffmpeg (over ffmpeg-free) for a better bootstrapping experience

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -10,7 +10,10 @@ is_macos(){
 }
 
 is_fedora(){
-  sudo dnf install ansible ansible-core curl git zsh -y
+  sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+  sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+  sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+  sudo dnf install 1password 1password-cli ansible ansible-core curl git zsh ffmpeg -y --allowerasing
   ansible-galaxy collection install community.general
 }
 
