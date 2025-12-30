ff19edfce6742f377741a6e189f38eafdcdd3bdc
Kartik Vashistha <kartikv.work@gmail.com>
2025-12-30 19:29:24 +0000

    fix(bootstrap.sh): fix switch case
    It was broken when trying to run it on a macos system (as no /etc/os-release file exists for macs).
    Additionally, also perform some fmt & linting changes as well.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,15 +1,17 @@
-# Simple bash script that bootstraps a fresh system with ansible and other necessary dependencies to run the many playbooks in this repo. Currently only supports bootstrapping Fedora and macOS machines, but can be extended quite easily for other linux distributions.
+#!/bin/bash
+# Simple bash script that bootstraps a fresh system with ansible and other necessary dependencies to run the many playbooks in this repo.
+# Currently only supports bootstrapping Fedora and macOS machines, but can be extended quite easily for other linux distributions.
 
-#/bin/bash
 set -e
 
-is_macos(){
+is_macos() {
+  echo "Detected a macOS system!"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   brew install ansible
   ansible-galaxy collection install community.general
 }
 
-is_fedora(){
+is_fedora() {
   sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
   sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
   sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
@@ -20,18 +22,16 @@ is_fedora(){
 is_linux() {
   release="$(cat /etc/os-release)"
 
-  if [[ $release =~ "Fedora Linux"  ]]; then
-    echo "Detected Fedora!" 
+  if [[ $release =~ "Fedora Linux" ]]; then
+    echo "Detected a Fedora system!"
     is_fedora
   else
-    "Unsupported Linux distribution :( "
+    echo "Unsupported Linux distribution!"
   fi
 }
 
-if [[ -f /etc/os-release ]]; then
-  case "$OSTYPE" in 
-    darwin*) is_macos ;;
-    linux-gnu) is_linux ;;
-    *) echo "unsupported OSTYPE: $OSTYPE" ;;
-  esac
-fi
+case "$OSTYPE" in
+darwin*) is_macos ;;
+linux-gnu) is_linux ;;
+*) echo "unsupported OSTYPE: $OSTYPE" ;;
+esac
