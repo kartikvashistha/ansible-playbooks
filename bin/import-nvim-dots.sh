b6359327f2ac301a8a79fadab0a1c5f7abfae4f7
Kartik Vashistha <kartikv.work@gmail.com>
2026-01-01 12:25:56 +0000

    chore(import-nvim-dots.sh): dont import lazy-lock.json

--- bin/import-nvim-dots.sh
+++ bin/import-nvim-dots.sh
@@ -5,3 +5,4 @@ set -e
 export NVIM_FILES_PATH="$HOME/code/github.com/personal/ansible-playbooks/roles/neovim/files/nvim"
 rm -r $NVIM_FILES_PATH/*
 cp -r ~/.config/nvim/* $NVIM_FILES_PATH
+rm $NVIM_FILES_PATH/lazy-lock.json
